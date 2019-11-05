62910b883851678a610bc9da3f41866fdc49f9a5
Christian Yores <christianyores@gmail.com>
2019-11-05 15:42:38 -0500

    Made document script produce more organized results

--- src/backend/scripts/document.js
+++ src/backend/scripts/document.js
@@ -1,9 +1,39 @@
 var documentation = require("documentation");
 var fs = require("fs");
 
-documentation
-    .build(["./src/app.js"], { shallow: false })
-    .then(documentation.formats.md)
-    .then(output => {
-        fs.writeFileSync("./README.md", output);
+let locations = [
+    { path: "./src/controllers/", ext: ".controller", title: "Controllers" },
+    { path: "./src/services/", ext: ".service", title: "Services" },
+    { path: "./src/db/", ext: ".db", title: "Database" }
+];
+
+for (let j = 0; j < locations.length; j++) {
+    let location = locations[j];
+    // console.log(location);
+    fs.readdir(location.path, async (err, files) => {
+        if (err) {
+            console.log("Error reading file", err);
+            process.exit(0);
+        }
+        let text = "# " + location.title;
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+            if (!file.includes(location.ext)) continue;
+            console.log("documenting", file);
+            let docs = await documentation.build([location.path + file], {
+                shallow: true
+            });
+            let format = await documentation.formats.md(docs);
+            let title =
+                "# " +
+                file
+                    .replace(".", " ")
+                    .replace(".", " ")
+                    .replace("js", "")
+                    .toUpperCase();
+            text += "\n" + title + "\n" + format + "\n";
+        }
+
+        fs.writeFileSync(location.path + "README.md", text);
     });
+}
